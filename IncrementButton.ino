a7692bc8e4f47ac22752a7c4ae6e9ba1d673411b
DevMorningCoffee <132598665+DEVMorningCoffee@users.noreply.github.com>
2025-06-15 18:27:08 -0400

    Increment value when button is clicked

---
+++ IncrementButton.ino
@@ -0,0 +1,30 @@
+const int pushButtonPin = 2;
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(pushButtonPin, INPUT);
+  Serial.begin(9600);
+
+}
+
+
+void loop() {
+  // put your main code here, to run repeatedly:
+   // LOW = Stop, HIGH = Go
+
+  int lastBtnState = LOW;
+  int buttonState = digitalRead(pushButtonPin);
+
+  int i = 0;
+
+  if(buttonState == HIGH && lastBtnState == LOW){
+    // lastBtnState = buttonState;
+      Serial.println(i);
+      delay(1);
+      i++;
+  }
+
+  lastBtnState = buttonState;
+  delay(50);
+
+}
